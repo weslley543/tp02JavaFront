aa38a1c61149446b71c87ac37c4db037e64af8a2
Luiz G <luiz13lg13@gmail.com>
2020-08-05 17:12:38 -0300

    Adicionando header

---
+++ src/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css'
+
+import user from '../img/user.png'
+
+function Header() {
+    return(
+        <div className="menu">
+            <p className="item-menu">Categorias</p>
+            <p className="item-menu">Favoritos</p>
+            <p className="item-menu">Mais Assistidos</p>
+            <div className="div-menu-usuario">
+                <img src={user} className="item-logo"/>
+                <p className="item-menu">Gustavo Serpeloni</p>
+            </div>
+        </div>
+    )
+  }
+  
+  export default Header;

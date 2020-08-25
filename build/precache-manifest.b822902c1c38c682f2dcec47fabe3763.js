e9d0ed7faddab8828a5f34ba28433897eb6676ef
Luiz G <luiz13lg13@gmail.com>
2020-08-25 20:49:59 -0300

    > Por favor, funciona

---
+++ build/precache-manifest.b822902c1c38c682f2dcec47fabe3763.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43fa7cf82aa978229d7d8da6fc264e1",
+    "url": "/tp02JavaFront/index.html"
+  },
+  {
+    "revision": "3c3070b74bc4983458b0",
+    "url": "/tp02JavaFront/static/css/main.15d6ec4f.chunk.css"
+  },
+  {
+    "revision": "02a1a79795602846c86f",
+    "url": "/tp02JavaFront/static/js/2.8faff45c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tp02JavaFront/static/js/2.8faff45c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c3070b74bc4983458b0",
+    "url": "/tp02JavaFront/static/js/main.96546833.chunk.js"
+  },
+  {
+    "revision": "c9fbf5806ee439ca301a",
+    "url": "/tp02JavaFront/static/js/runtime-main.b660f630.js"
+  },
+  {
+    "revision": "0c3b1f082d6455575fccdf6ebe8f0fb9",
+    "url": "/tp02JavaFront/static/media/logo.0c3b1f08.png"
+  }
+]);

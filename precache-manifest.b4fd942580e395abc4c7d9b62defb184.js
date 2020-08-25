a85305ca649226c408891b9e1a92781ec62fc703
Gustavo Serpeloni <gserpeloni@hotmail.com>
2020-08-25 18:19:37 -0300

    Updates

---
+++ precache-manifest.b4fd942580e395abc4c7d9b62defb184.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f622b1c9e2a84fabee420e48b718c28c",
+    "url": "/tp02JavaFront/index.html"
+  },
+  {
+    "revision": "2f397f5aed009d0dca93",
+    "url": "/tp02JavaFront/static/css/main.3b5a30c7.chunk.css"
+  },
+  {
+    "revision": "8bfaa93e31143a453b99",
+    "url": "/tp02JavaFront/static/js/2.695427a8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tp02JavaFront/static/js/2.695427a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f397f5aed009d0dca93",
+    "url": "/tp02JavaFront/static/js/main.7b8a770b.chunk.js"
+  },
+  {
+    "revision": "c9fbf5806ee439ca301a",
+    "url": "/tp02JavaFront/static/js/runtime-main.b660f630.js"
+  },
+  {
+    "revision": "0c3b1f082d6455575fccdf6ebe8f0fb9",
+    "url": "/tp02JavaFront/static/media/logo.0c3b1f08.png"
+  }
+]);
